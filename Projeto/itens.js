6470b01763783429f46b3eda92176f0966c0d8a9
Felipe Alencar <fsalencar@outlook.com>
2016-12-01 10:44:30 -0200

    Criando um arquivo separado para ler os itens em js

---
+++ Projeto/itens.js
@@ -0,0 +1,10 @@
+var itens = $(".item-total");
+var total = 0;
+	for(var i = 0; i < itens.length ; i++){
+			var item = $(itens[i]);
+			var valor = parseFloat(item.text());
+			total = total + valor;
+		}
+		console.log("Valor total é: " + total);
+		$("#valor-total").text(total);
+		$("#quantidade-de-itens").text(itens.length);

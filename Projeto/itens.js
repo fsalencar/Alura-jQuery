f987dd05522877c4b4eea0678a7a36fbe4718cfa
fsalencar <fsalencar@outlook.com>
2016-12-02 21:55:30 -0200

    Adicionando função de remover itens do carrinho, no arquivo jQuery (diferente do curso)

--- Projeto/itens.js
+++ Projeto/itens.js
@@ -1,13 +1,26 @@
-var aposInicializado = function(){
-var itens = $(".item-total");
-var total = 0;
-	for(var i = 0; i < itens.length ; i++){
-			var item = $(itens[i]);
-			var valor = parseFloat(item.text());
-			total = total + valor;
-		}
-		console.log("Valor total é: " + total);
-		$("#valor-total").text(total);
-		$("#quantidade-de-itens").text(itens.length);
+var removeItem = function(event){
+	var self = $(this);
+	self.closest("tr").remove();
+
+	event.preventDefault();
+	var atual = parseInt($("#quantidade-de-itens").text());
+	var novaQuantidade = atual - 1;
+	$("#quantidade-de-itens").text(novaQuantidade);
+
 };
+
+var aposInicializado = function(){
+	var itens = $(".item-total");
+	var total = 0;
+		for(var i = 0; i < itens.length ; i++){
+				var item = $(itens[i]);
+				var valor = parseFloat(item.text());
+				total = total + valor;
+			}
+
+			console.log("Valor total é: " + total);
+			$("#valor-total").text(total);
+			$("#quantidade-de-itens").text(itens.length);
+			$(".remove-item").click(removeItem);
+	};
 $(aposInicializado);

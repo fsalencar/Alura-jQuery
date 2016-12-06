55cf0c8a3b51008f31c802ab6359a476f28c58c6
Felipe Alencar <fsalencar@outlook.com>
2016-12-06 09:03:05 -0200

    Atualizando o css e adicionando o botão de desfazer

--- Projeto/itens.js
+++ Projeto/itens.js
@@ -1,27 +1,35 @@
 var removeItem = function(event){
 	event.preventDefault();
-	var self = $(this);
-	self.closest("tr").remove();
 
+	var self = $(this);
+	self.closest("tr").hide();
 
 	atualizaDados()
-
 };
+
 var atualizaDados = function(){
-	var itens = $(".item-total");
+	var itens = $(".item-total:visible");
 	var total = 0;
 		for(var i = 0; i < itens.length ; i++){
 				var item = $(itens[i]);
 				var valor = parseFloat(item.text());
 				total = total + valor;
 			}
-
 			console.log("Valor total é: " + total);
 			$("#valor-total").text(total);
 			$("#quantidade-de-itens").text(itens.length);
 }
+
+var undo= function(){
+	$("tr:visible").removeClass("recuperado");
+	var trs = $("tr:hidden");
+	trs.addClass("recuperado");
+	trs.show();
+}
+
 var aposInicializado = function(){
 	atualizaDados();
+	$("#undo").click(undo);
 	$(".remove-item").click(removeItem);
 	};
 $(aposInicializado);

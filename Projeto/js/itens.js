3d9b431c07e454e4c2a50c8894badd84e90fa96b
Felipe Alencar <fsalencar@outlook.com>
2016-12-08 16:55:42 -0200

    Trabalhando com .class e funções anonimas

--- Projeto/js/itens.js
+++ Projeto/js/itens.js
@@ -8,28 +8,34 @@ var removeItem = function(event){
 };
 
 var atualizaDados = function(){
-	var itens = $(".item-total:visible");
-	var total = 0;
-		for(var i = 0; i < itens.length ; i++){
-				var item = $(itens[i]);
-				var valor = parseFloat(item.text());
-				total = total + valor;
-			}
-			console.log("Valor total é: " + total);
-			$("#valor-total").text(total);
-			$("#quantidade-de-itens").text(itens.length);
+	var carrinhos =$(".carrinho");
+	carrinhos.each(function(){
+				var carrinho =$(this);
+				var itens = carrinho.find(".item-total:visible");
+				var total = 0;
+					for(var i = 0; i < itens.length ; i++){
+							var item = $(itens[i]);
+							var valor = parseFloat(item.text());
+							total = total + valor;
+						}
+						console.log("Valor total é: " + total);
+						carrinho.find(".valor-total").text(total);
+						carrinho.find(".quantidade-de-itens").text(itens.length);
+			});
 }
 
 var undo= function(){
-	$("tr:visible").removeClass("recuperado");
-	var trs = $("tr:hidden");
-	trs.addClass("recuperado");
-	trs.show();
+	var carrinho = $(this).closest(".carrinho");
+	carrinho.find("tr:visible").removeClass("recuperado");
+
+	var trs = carrinho.find("tr:hidden");
+	trs.addClass("recuperado").show();
+	atualizaDados();
 }
 
 var aposInicializado = function(){
 	atualizaDados();
-	$("#undo").click(undo);
+	$(".undo").click(undo);
 	$(".remove-item").click(removeItem);
 	};
 $(aposInicializado);
